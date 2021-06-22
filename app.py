db0e67949955131fa76184d6744e3acf021d5d0b
lachaal <osamaloup@gmail.com>
2021-06-22 13:39:39 +0100

    display the endpoint information

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return """
+        <p>
+            Available Endpoints: <br />
+            <strong style="margin-left:3em;">/fetch:</strong>
+            <span style="margin-left:5em;">return a json object which list all languages with repositories</span>
+        </p>
+        """
 
 @app.route("/fetch")
 def fetch():
